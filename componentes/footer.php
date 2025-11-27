e6de69b09437df9c602e6a225b61c1a2455d433b
davichorodasw <david.rodas@es.uazuay.edu.ec>
2025-11-27 07:40:17 -0500

    Actualización links footer

--- componentes/footer.php
+++ componentes/footer.php
@@ -6,13 +6,21 @@
         </div>
         <div class="footer-section">
             <h3>Enlaces Rápidos</h3>
-            <a href="./index.php">Inicio</a>
-            <a href="./views/cds.php">CDs</a>
-            <a href="./views/vinilos.php">Vinilos</a>
+            <a href="<?php echo isset($basePath)
+                            ? $basePath . "index.php"
+                            : "./index.php"; ?>">Inicio</a>
+            <a href="<?php echo isset($basePath)
+                            ? $basePath . "views/cds.php"
+                            : "./views/cds.php"; ?>">CDs</a>
+            <a href="<?php echo isset($basePath)
+                            ? $basePath . "views/vinilos.php"
+                            : "./views/vinilos.php"; ?>">Vinilos</a>
         </div>
         <div class="footer-section">
             <h3>Contacto</h3>
-            <a href="/contacto">Formulario de contacto</a>
+            <a href="<?php echo isset($basePath)
+                            ? $basePath . "views/contacto.php"
+                            : "./views/contacto.php"; ?>">Contacto</a>
             <p>email@ritmoretro.com</p>
             <p>+593 98 765 4321</p>
         </div>
@@ -22,4 +30,5 @@
     </div>
 </footer>
 </body>
+
 </html>
--- componentes/nav.php
+++ componentes/nav.php
@@ -1,32 +1,32 @@
 <nav>
     <a href="<?php echo isset($basePath)
-        ? $basePath . "index.php"
-        : "./index.php"; ?>" <?php echo isset($currentPage) &&
-$currentPage === "inicio"
-    ? 'class="active"'
-    : ""; ?>>Inicio</a>
+                    ? $basePath . "index.php"
+                    : "./index.php"; ?>" <?php echo isset($currentPage) &&
+                                    $currentPage === "inicio"
+                                    ? 'class="active"'
+                                    : ""; ?>>Inicio</a>
     <a href="<?php echo isset($basePath)
-        ? $basePath . "views/cds.php"
-        : "./views/cds.php"; ?>" <?php echo isset($currentPage) &&
-$currentPage === "cds"
-    ? 'class="active"'
-    : ""; ?>>CDs</a>
+                    ? $basePath . "views/cds.php"
+                    : "./views/cds.php"; ?>" <?php echo isset($currentPage) &&
+                                        $currentPage === "cds"
+                                        ? 'class="active"'
+                                        : ""; ?>>CDs</a>
     <a href="<?php echo isset($basePath)
-        ? $basePath . "views/vinilos.php"
-        : "./views/vinilos.php"; ?>" <?php echo isset($currentPage) &&
-$currentPage === "vinilos"
-    ? 'class="active"'
-    : ""; ?>>Vinilos</a>
+                    ? $basePath . "views/vinilos.php"
+                    : "./views/vinilos.php"; ?>" <?php echo isset($currentPage) &&
+                                            $currentPage === "vinilos"
+                                            ? 'class="active"'
+                                            : ""; ?>>Vinilos</a>
     <a href="<?php echo isset($basePath)
-        ? $basePath . "views/login.php"
-        : "./views/login.php"; ?>" <?php echo isset($currentPage) &&
-$currentPage === "login"
-    ? 'class="active"'
-    : ""; ?>>Iniciar sesión</a>
+                    ? $basePath . "views/login.php"
+                    : "./views/login.php"; ?>" <?php echo isset($currentPage) &&
+                                        $currentPage === "login"
+                                        ? 'class="active"'
+                                        : ""; ?>>Iniciar sesión</a>
     <a href="<?php echo isset($basePath)
-        ? $basePath . "views/contacto.php"
-        : "./views/contacto.php"; ?>" <?php echo isset($currentPage) &&
-$currentPage === "contacto"
-    ? 'class="active"'
-    : ""; ?>>Contacto</a>
+                    ? $basePath . "views/contacto.php"
+                    : "./views/contacto.php"; ?>" <?php echo isset($currentPage) &&
+                                            $currentPage === "contacto"
+                                            ? 'class="active"'
+                                            : ""; ?>>Contacto</a>
 </nav>
