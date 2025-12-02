293d6386ea731334eee27ec16b1931d7e14d5860
davichorodasw <david.rodas@es.uazuay.edu.ec>
2025-12-02 09:32:11 -0500

    Emojis

--- componentes/nav.php
+++ componentes/nav.php
@@ -34,7 +34,7 @@ $user_name = $logged_in ? $_SESSION['user_name'] : '';
         <!-- Menú desplegable para usuario logueado -->
         <div class="dropdown">
             <a href="#" class="dropbtn">
-                <span class="user-icon">👤</span>
+                <!-- <span class="user-icon">👤</span> -->
                 <span class="user-name"><?php echo htmlspecialchars($user_name); ?></span>
                 <span class="dropdown-arrow">▼</span>
             </a>
@@ -43,12 +43,12 @@ $user_name = $logged_in ? $_SESSION['user_name'] : '';
                                 ? $basePath . 'views/mi-cuenta.php'
                                 : './views/mi-cuenta.php'; ?>"
                     <?php echo isset($currentPage) && $currentPage === 'mi-cuenta' ? 'class="active"' : ''; ?>>
-                    <span class="dropdown-icon">📋</span> Mi Cuenta
+                    Mi Cuenta
                 </a>
                 <a href="<?php echo isset($basePath)
                                 ? $basePath . 'php/logout.php'
                                 : './php/logout.php'; ?>">
-                    <span class="dropdown-icon">🚪</span> Cerrar Sesión
+                    Cerrar Sesión
                 </a>
             </div>
         </div>
@@ -62,14 +62,13 @@ $user_name = $logged_in ? $_SESSION['user_name'] : '';
         </a>
     <?php endif; ?>
 
-    <a href="<?php echo isset($basePath)
-                    ? $basePath . 'views/contacto.php'
-                    : './views/contacto.php'; ?>"
+    <!-- <a href="<?php echo isset($basePath)
+                        ? $basePath . 'views/contacto.php'
+                        : './views/contacto.php'; ?>"
         <?php echo isset($currentPage) && $currentPage === 'contacto' ? 'class="active"' : ''; ?>>
         Contacto
-    </a>
+    </a> -->
 </nav>
 
-<!-- Incluir CSS y JS del dropdown -->
 <link rel="stylesheet" href="<?php echo isset($basePath) ? $basePath . 'css/dropdown.css' : '../css/dropdown.css'; ?>" />
 <script src="<?php echo isset($basePath) ? $basePath . 'js/dropdown.js' : '../js/dropdown.js'; ?>" defer></script>
